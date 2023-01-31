fac71ed6857b2e70106443f91ffdb13dfdd8b458
Stanislav Razin <109966035+Dizzy14@users.noreply.github.com>
2023-01-31 14:26:37 +0300

    Add files via upload

---
+++ loss.py
@@ -0,0 +1,32 @@
+"""
+Данный модуль предназначен исключительно для ознакомления с тем,
+как работает loss в обучении и никак не задействован в работе "SRGAN telegram-bot"
+"""
+
+import torch.nn as nn
+import torch.nn.functional as F
+
+from torch import Tensor
+from torchvision.models import vgg19
+
+
+class PerceptionLoss(nn.Module):
+    """
+    Класс, который считает MSE между двумя тензорами в пространстве фичей VGG19
+    """
+
+    def __init__(self, feature_layer: int = 35):
+        super(PerceptionLoss, self).__init__()
+        model = vgg19(pretrained=True)
+        self.features = nn.Sequential(*list(model.features.children())[:feature_layer]).eval()
+
+        # отключаем подсчет градиентов внутри vgg
+        for name, param in self.features.named_parameters():
+            param.requires_grad = False
+
+    def forward(self, input: Tensor, target: Tensor) -> Tensor:
+        # вычисляем лосс в пространстве фичей vgg19
+        perception_loss = F.mse_loss(self.features(input), self.features(target))
+
+        return perception_loss
+
